9e4e0fafc4013302808eff42929bd47166b16245
snkchan <144839872+snkchan@users.noreply.github.com>
2023-12-31 03:01:27 +0900

    feat: debounce사용해서 데이터 패칭로직 작성

--- src/components/search_area.jsx
+++ src/components/search_area.jsx
@@ -1,14 +1,31 @@
 import styled from "styled-components"
 import { color, flexAlign, fontSize, fontWeight } from "../styles/themes"
 import SearchList from "./search_list"
+import UseDebounce from "../hooks/use_debounce"
+import useFetchKeywords from "../hooks/use_fetch_keywords"
 
 const SearchArea = () => {
+  const { searchData, fetchSearchList } = useFetchKeywords()
+  const getInputText = UseDebounce(fetchSearchList, 500)
+
+  const onKeyUp = (e) => {
+    getInputText(e.target.value)
+  }
+
+  console.log(searchData)
+
   return (
     <S.Div_MainWrap>
       <S.Div_FormWrap>
         <S.For_Form>
-          <S.Inp_Input />
-          <S.But_Button>🔍</S.But_Button>
+          <S.Inp_Input onChange={onKeyUp} />
+          <S.But_Button
+            onClick={(e) => {
+              e.preventDefault()
+            }}
+          >
+            🔍
+          </S.But_Button>
         </S.For_Form>
         <SearchList />
       </S.Div_FormWrap>
---
+++ src/hooks/use_debounce.js
@@ -0,0 +1,11 @@
+const UseDebounce = (callback, delay) => {
+  let timer
+  return (...args) => {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => callback(...args), delay)
+  }
+}
+
+export default UseDebounce
